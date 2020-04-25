6f5d6e3c04ed2c4f405b37356334864f2aad0cec
dan7sc <su7cheng@gmail.com>
2020-04-24 21:38:24 -0300

    Add solution to exercise 3.2: Phonebook backend step2

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ const PERSONS = require('./db.json')
 const app = express()
 const PORT = 3001
 
+app.get('/info', (req, res) => {
+    const persons = PERSONS.persons
+    const size = persons.length
+    const date = new Date().toString()
+    const response = `<p>Phonebook has info for ${size} people</p>` +
+          `<p>${date}</p>`
+    res.send(response)
+})
+
 app.get('/api/persons', (req, res) => {
     res.json(PERSONS)
 })
