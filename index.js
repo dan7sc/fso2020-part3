c58b31c9579c4ca77f0755ac2897bad7a8f9ed4e
dan7sc <su7cheng@gmail.com>
2020-04-24 21:21:54 -0300

    Add solution to exercise 3.1: Phonebook backend step1
    Acked-by: dan7sc <su7cheng@gmail.com>

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('hello world')
+const express = require('express')
+const PERSONS = require('./db.json')
+
+const app = express()
+const PORT = 3001
+
+app.get('/api/persons', (req, res) => {
+    res.json(PERSONS)
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
