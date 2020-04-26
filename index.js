6060cb4642e93b0e963690a35a146967de88f36f
dan7sc <su7cheng@gmail.com>
2020-04-25 21:46:25 -0300

    Add solution to exercise 3.10 phonebook backend step10

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 let persons = require('./db.json')
 
 const app = express()
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 
 morgan.token('type', (req, res) => {
     return JSON.stringify(req.body)
